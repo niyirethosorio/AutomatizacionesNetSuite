4ccca7829cae1137ee32159d9f86225fdd493c14
niyirethosorio <niyirethosorio@gmail.com>
2021-04-21 14:09:04 -0500

    Parametrización de datos
    Parametrizar datos de entrada a la página

--- BDD/Features/NetSuite_Tarifas.feature
+++ BDD/Features/NetSuite_Tarifas.feature
@@ -1,6 +1,9 @@
 Feature: Validar tarifas
 
 Scenario: Validar tarifa del servicio
-  Given Se loguea a NetSuite con usuario "admin" y contraseña "BennyH1ll" y empresa "Comfama"
+  Given ingresar al ambiente de "pruebas" de netsuite
+  Then Loquearse con usuario "admin", contraseña "BennyH1ll" y empresa "Comfama"
+
+
 
 
--- BDD/Features/Steps/Netsuite_Pages.py
+++ BDD/Features/Steps/Netsuite_Pages.py
@@ -1,6 +1,14 @@
 from behave import *
 
 
-@given('Se loguea a NetSuite con usuario {user} y contraseña {password} y empresa {enterprise}')
+@given('ingresar al ambiente de {environment} de netsuite')
+def step_impl(context, environment):
+    if environment is "pruebas":
+        context.browser.get("https://reservas.viajescomfama.com/NetAdmin/Login.aspx")
+    elif environment is "google":
+        context.browser.get("https://www.google.com/")
+
+
+@then('Loquearse con usuario {user}, contraseña {password} y empresa {enterprise}')
 def step_impl(context, user, password, enterprise):
-    context.browser.get("https://reservas.viajescomfama.com/NetAdmin/Login.aspx")
+    pass
